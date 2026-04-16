cfc21b8cac1e13e7c3b171782a7e786d995f86a6
Gabriel Assis <cardimg06@gmail.com>
2026-04-16 16:24:30 -0300

    feat: add list patients dto response

---
+++ src/Services/Patients/HealthSync.Services.Patients/Features/v1/ListPatients/Dtos/ListPatientsResponse.cs
@@ -0,0 +1,13 @@
+namespace HealthSync.Services.Patients.Features.v1.GetPatient.Dtos
+{
+    public class ListPatientsResponse
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
