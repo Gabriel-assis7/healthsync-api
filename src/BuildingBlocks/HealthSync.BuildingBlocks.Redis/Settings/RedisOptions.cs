d6f060967b55dc63d03934efdf3e683a3eac40dc
Gabriel Assis <cardimg06@gmail.com>
2026-05-01 16:31:23 -0300

    feat(redis): add settings options

---
+++ src/BuildingBlocks/HealthSync.BuildingBlocks.Redis/Settings/RedisOptions.cs
@@ -0,0 +1,20 @@
+using HealthSync.BuildingBlocks.Resiliency.Options;
+
+namespace HealthSync.BuildingBlocks.Redis.Settings;
+
+public class RedisOptions
+{
+    public bool? Enabled { get; set; }
+    public string? ClientName { get; set; }
+    public string? User { get; set; }
+    public string? Password { get; set; }
+    public bool EnableSsl { get; set; } = true;
+    public string? Host { get; set; }
+    public int Port { get; set; } = 6379;
+    public bool? AbortOnConnectFail { get; set; }
+    public int ConnectTimeout { get; set; } = 5000;
+    public int ConnectRetry { get; set; } = 3;
+    public RetryPolicyOptions? ReconnectRetryPolicy { get; set; }
+    public int SyncTimeout { get; set; } = 5000;
+    public int AsyncTimeout { get; set; } = 5000;
+}
